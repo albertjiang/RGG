22211d3fdfae9c7861c594be6bcd1fdec0f884c2
Evan <wgarvie@trinity.edu>
2015-10-15 14:39:56 -0500

    Fixing tabs

--- rgg.cc
+++ rgg.cc
@@ -10,32 +10,32 @@ tuple<bool, int> isFeasible(int playerID, pureStrategy p) {
 }
 
 double getPureStrategyUtility(int playerID, pureStrategyProfile &p) {
-	double u=0;
-	vector<int> totalConfig(numResourceNodes,0);
-	//Going through the pure strategy profile, and adding 1 to
-	//total config value at index whenever a resource node is
-	//found in the profile.
-	for(int a=0; a<p.size(); a++){
-		for(int b=0; b<p[a].size(); b++) {
-			totalConfig[pureStrategyProfile[a][b]] += 1;
-		}
-	}
-	for(int i=0; i<numResourceNodes; i++) {
-			if(p[playerId][i]) {
-				vector<int> localConfig(neighbors[i].size);
-				for(int j=0; j<neighbors[i].size(); j++) {
-					localConfig[j] = totalConfig[neighbors[i][j]];
-				}
-				trie_map<double> p = findExact(localConfig);
-				if(currMap == data.end()) {
-					cout << "Error! Local Configuration Not Found";
-					exit(1);
-				} else {
-					u += p->second;
-				}
-			}
-	}
-	return u;
+  double u=0;
+  vector<int> totalConfig(numResourceNodes,0);
+  //Going through the pure strategy profile, and adding 1 to
+  //total config value at index whenever a resource node is
+  //found in the profile.
+  for(int a=0; a<p.size(); a++){
+    for(int b=0; b<p[a].size(); b++) {
+      totalConfig[pureStrategyProfile[a][b]] += 1;
+    } 
+  }
+  for(int i=0; i<numResourceNodes; i++) {
+    if(p[playerId][i]) {
+      vector<int> localConfig(neighbors[i].size);
+      for(int j=0; j<neighbors[i].size(); j++) {
+        localConfig[j] = totalConfig[neighbors[i][j]];
+      }
+      trie_map<double> p = findExact(localConfig);
+        if(currMap == data.end()) {
+          cout << "Error! Local Configuration Not Found";
+          exit(1);
+        } else {
+          u += p->second;
+        }
+      }
+  }
+  return u;
 }
 
 
